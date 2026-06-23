cd56f2d0b0d5a7674c951898575fd4c0675b4c92
Veesam Mohan <veesammallarao3@gmail.com>
2026-06-23 11:30:11 +0530

    Initial commit

---
+++ notification-app-fe/src/components/NotificationFilter.jsx
@@ -0,0 +1,23 @@
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+const filters = ["All", "Placement", "Result", "Event"];
+export function NotificationFilter({ value, onChange }) {
+  return (
+    <ToggleButtonGroup
+      value={value}
+      exclusive
+      size="small"
+      onChange={(_, newFilter) => onChange(newFilter)}
+      sx={{ flexWrap: "wrap", gap: 0.5 }}
+    >
+      {filters.map((type) => (
+        <ToggleButton
+          key={type}
+          value={type}
+          sx={{ textTransform: "none", px: 2 }}
+        >
+          {type}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  );
+}
